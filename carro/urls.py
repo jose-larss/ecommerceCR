b8c62d38ae23d34a30755dad0d8e61efe95e4b9d
jlcastro <joselucianocastro10@gmail.com>
2023-09-08 23:07:20 +0200

    implemento sesiones V1

--- carro/urls.py
+++ carro/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from carro.views import vista, update_to_cart
+from carro.views import vista, update_to_cart, borrar_carro
 
 app_name = "carro"
 
 urlpatterns = [
     path('', vista, name="vista"),
+    path('delete/', borrar_carro, name="borrar-carro"),
     path('<slug:slug>/',update_to_cart, name="update_to_cart"),
+    
 ]
--- carro/views.py
+++ carro/views.py
@@ -5,20 +5,49 @@ from carro.models import Carro
 
 def vista(request):
     msj_articulos = ""
-    carro = Carro.objects.all()[0]
+    carro_vacio = False
+    
+    try:
+        the_id = request.session['carro_id']
+    except:
+        the_id = None
+    print(the_id)
+    if the_id:
+        carro = Carro.objects.get(id=the_id)
+        
+        if carro.productos.all().count() == 0:
+            carro_vacio = True
+        elif carro.productos.all().count() == 1:
+            msj_articulos = f"{carro.productos.all().count()} artículo"
+        elif carro.productos.all().count() > 1:
+            msj_articulos = f"{carro.productos.all().count()} artículos"
 
-    if carro.productos.all().count() == 1:
-        msj_articulos = f"{carro.productos.all().count()} artículo"
-    elif carro.productos.all().count() > 1:
-        msj_articulos = f"{carro.productos.all().count()} artículos"
-    context = {
-        "carro": carro,
-        "msj_articulos":msj_articulos
-    }
+        context = {
+            "carro": carro,
+            "carro_vacio":carro_vacio,
+            "msj_articulos":msj_articulos
+        }
+    else:
+        context = {
+            'carro_vacio': True
+        }
+
     return render(request, "carros/vista.html", context)
 
+
 def update_to_cart(request, slug):
-    carro = Carro.objects.all()[0]
+    nuevo_total = 0.00
+    
+    try:
+        the_id = request.session['carro_id']
+    except:
+        nuevo_carro = Carro()
+        nuevo_carro.save()
+        request.session['carro_id'] = nuevo_carro.id
+        the_id = nuevo_carro.id
+    print(the_id)
+
+    carro = Carro.objects.get(id=the_id)
     producto = get_object_or_404(Producto, slug=slug)
 
     if not producto in carro.productos.all():
@@ -26,7 +55,6 @@ def update_to_cart(request, slug):
     else:
         carro.productos.remove(producto)
     
-    nuevo_total = 0.00
     for item in carro.productos.all():
         nuevo_total += float(item.precio)
     
@@ -34,4 +62,18 @@ def update_to_cart(request, slug):
     carro.total = nuevo_total
     carro.save()
     
+    return redirect("carro:vista")
+
+
+def borrar_carro(request):
+    the_id = request.session['carro_id']
+    carro = Carro.objects.get(id=the_id)
+    """
+    for producto in carro.productos.all():
+        carro.productos.remove(producto)
+    """
+    carro.productos.clear()
+    carro.total = 0
+    carro.save()
+        
     return redirect("carro:vista")
